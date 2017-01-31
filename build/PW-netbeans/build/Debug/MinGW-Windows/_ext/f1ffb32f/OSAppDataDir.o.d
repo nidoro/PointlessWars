b059966ba0904f58929ab7bd950604055dc302be
davidoro <davi_doro@hotmail.com>
2017-01-31 21:13:53 -0200

    Fixed project tree

--- build/PW-netbeans/build/Debug/MinGW-Windows/_ext/f1ffb32f/OSAppDataDir.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/MinGW-Windows/_ext/f1ffb32f/OSAppDataDir.o: \
- c:/Users/Davi\ Doro/PW\ Local/PointlessWars/code/OSAppDataDir.cpp \
- c:/Users/Davi\ Doro/PW\ Local/PointlessWars/code/OSAppDataDir.h
-
-c:/Users/Davi\ Doro/PW\ Local/PointlessWars/code/OSAppDataDir.h:
